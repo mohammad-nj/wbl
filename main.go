8b4e178310010d9eba599628ea1a57c09f5aefa0
Mohammad Najjary <mohammad.najjary@arvancloud.com>
2023-10-04 19:08:46 +0330

    feat: first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"git.arvaninternal.ir/argo/lang"
+)
+
+func main(){
+    lang.SetTranslationFilesPath("translate")
+    lang.SetDefaultLocale("fa")
+    lang.SetFallbackLocales("en")
+    lang.Init()
+
+	fmt.Println(lang.TrBy("fa", "msg.wallet_created"))
+	fmt.Println(lang.TrBy("en", "msg.wallet_created"))
+
+}
